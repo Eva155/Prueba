564e8987c3a2d568d35b1e75322a68c550a30769
Eva155 <aguilar19eva@gmail.com>
2021-05-19 12:56:06 -0500

    Subiendo archivos de enlaces

---
+++ enlaces/CSS/estilos.css
@@ -0,0 +1,76 @@
+/*
+CONSIDERACIONES PROGRAMAR HOJAS DE ESTILOS
+1. Primero se definen los estilos para ETIQUETAS html, por ejemplo: header, footer, div, a, p, etc.
+2. Después se definen los estilos para IDENTIFICADORES (id), los id´s son elemento únicos en el html, en el css se declaran como el símbolo de # ejemplo:
+#nombre-id(estilos) y en el html se llaman mediante el atributo id ejemplo <div id=”publicidad”><div>
+3. Al final se definen los estilos para las CLASES (class), las clases son elementos que se pueden repetir ‘n’ veces en el html, en el css se declaran con
+el símbolo de . ejemplo: .nombre-clase{estilos} y en el html se llaman mediante el atributo class, ejemplo <div class=”borde-logos></div>
+4. Todos los estilos se ordenan conforme van apareciendo en la disposición del html, teniendo en cuenta la prioridad anterior, es decir, primero etiquetas 
+html, luego id´s y al final clases.
+5. Los atributos se declaran de la siguiente forma: {atributo1:valor1; atributo2:valor2;…;atributoN:valorN;}
+6. Todos los atributos se ordenan alfabéticamente, por ejemplo si tengo width, color, display, entonces el orden seria: primero color, luego display y al
+final width.
+7. Si tenemos estilos para el comodín *, o para las etiquetas html y/o body, estos estilos son los primeros en definirse.
+8. En el body debemos declarar un tamaño de fuente global.
+9. Si tenemos estilos para resetear elementos html, estos irán antes de cualquier estilo. 
+*{
+margin: 0;
+padding: 0;
+}
+*/
+
+*{
+	margin: 0;
+	padding: 0;
+}
+/* 16px=12pt=100% =1em*/
+body {
+	background: #212121;
+	color: #FFF;
+	font-family: "soberana", Arial;
+	/*Responsive desing*/
+	font-size: 2em;
+}
+h1 {
+	color: #F60;
+	margin: 0.25em auto;
+	text-align:center;
+	/*sombra*/
+	text-shadow: 5px 5px 10px rgba(255,255,255,0.9);
+}
+header,nav,footer {
+	background: #1E6381;
+	border-radius: 0.5em;
+	margin: 0.5em auto;
+	max-width: 960px;
+	padding: 0.25em;
+	text-align: center;
+}
+section#contenedor {
+	/*background: #FFF;*/
+	margin: 0.5em auto;
+	max-width: 960px;
+	padding: 0;
+	text-align: center;
+}
+section#principal,aside {
+	background: #1E6381;
+	border-radius: 0.5em;
+	display: inline-block;
+	margin: 0.25em auto;
+	max-width: 960px;
+	min-height: 200px;
+	padding: 0.25em;
+	text-align: center;
+	vertical-align: top;
+	width: 65%;
+}
+article {
+	background: #011E30;
+	border-radius: 0.5em;
+	min-height: 140px;
+	padding: 0.25em;
+}
+aside {
+	width: 30%;
+}
